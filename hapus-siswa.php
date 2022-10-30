bf808ce8c592b612f13e5348553a3108059a707d
vada <vada.annisaekasetya@gmail.com>
2022-10-30 17:23:36 +0700

    neat2

--- hapus-siswa.php
+++ hapus-siswa.php
@@ -1,12 +1,12 @@
 <?php
-include('koneksi.php');
+    include('koneksi.php');
 
-$id = $_GET['id'];
-$query = "DELETE FROM tbl_siswa WHERE id_siswa = '$id'";
+    $id = $_GET['id'];
+    $query = "DELETE FROM tbl_siswa WHERE id_siswa = '$id'";
 
-if($koneksi->query($query)) {
-    header("location: index.php");
-} else {
-    echo "<script>alert('Data Gagal Dihapus!')</script>";
-}
+    if($koneksi->query($query)) {
+        header("location: index.php");
+    } else {
+        echo "<script>alert('Data Gagal Dihapus!')</script>";
+    }
 ?> 
--- update-siswa.php
+++ update-siswa.php
@@ -1,16 +1,16 @@
 <?php
-include('koneksi.php');
+    include('koneksi.php');
 
-$id_siswa     = $_POST['id_siswa'];
-$nisn         = $_POST['nisn'];
-$nama_lengkap = $_POST['nama_lengkap'];
-$alamat       = $_POST['alamat'];
+    $id_siswa     = $_POST['id_siswa'];
+    $nisn         = $_POST['nisn'];
+    $nama_lengkap = $_POST['nama_lengkap'];
+    $alamat       = $_POST['alamat'];
 
-$query = "UPDATE tbl_siswa SET nisn = '$nisn', nama_lengkap = '$nama_lengkap', alamat = '$alamat' WHERE id_siswa = '$id_siswa'";
+    $query = "UPDATE tbl_siswa SET nisn = '$nisn', nama_lengkap = '$nama_lengkap', alamat = '$alamat' WHERE id_siswa = '$id_siswa'";
 
-if($koneksi->query($query)) {
-    header("location: index.php");
-} else {
-    echo "<script>alert('Data Gagal Diupdate!')</script>";
-}
+    if($koneksi->query($query)) {
+        header("location: index.php");
+    } else {
+        echo "<script>alert('Data Gagal Diupdate!')</script>";
+    }
 ?> 
