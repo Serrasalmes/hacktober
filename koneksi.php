19d7006e9e421e6b17f06cff502f5febacddb6f9
vada <vada.annisaekasetya@gmail.com>
2022-10-30 17:24:25 +0700

    neat2

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,7 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","my_website");
+	$koneksi = mysqli_connect("localhost","root","","my_website");
 
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
+	if (mysqli_connect_errno()){
+		echo "Koneksi database gagal : " . mysqli_connect_error();
+	}
 ?>
