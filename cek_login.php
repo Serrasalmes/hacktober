0f087c2a0dc7feffb4e1b42c5575d82d28e09938
vada <vada.annisaekasetya@gmail.com>
2022-10-30 17:21:50 +0700

    neat

--- cek_login.php
+++ cek_login.php
@@ -6,7 +6,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
  
 $data = mysqli_query($koneksi,"select * from admin where username='$username' and password='$password'");
-$cek = mysqli_num_rows($data);
+$cek  = mysqli_num_rows($data);
  
 if($cek > 0){
 	$_SESSION['username'] = $username;
